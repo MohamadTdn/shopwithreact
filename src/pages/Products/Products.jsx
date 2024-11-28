4cbbe6a736228332f81b838446f2a2838719c726
MohamadTdn <mohamadmt1579@gmail.com>
2024-11-28 19:30:59 +0330

    finish products page

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import React from "react";
+import NavMenu from "../../components/NavMenu/NavMenu";
+import Footer from "../../components/Footer/Footer";
+import { Container, Row, Col } from "react-bootstrap";
+import ProductBox from "../../components/ProductBox/ProductBox";
+import './Products.css'
 
 export default function Products() {
   return (
-    <div>Products</div>
-  )
+    <div className="Products">
+      <NavMenu />
+      <select className="filter-box">
+        <option value="-1">Choose the filter</option>
+        <option value="cheap">Cheap</option>
+        <option value="expensive">Expensive</option>
+        <option value="shoes">Shoes</option>
+        <option value="gadget">Gadget</option>
+      </select>
+      <Container>
+        <Row>
+          <Col lg={4} md={6} sm={12}>
+            <ProductBox
+              title={`airpod`}
+              price={200}
+              imgSrc={`images/airpodimg.webp`}
+            />
+          </Col>
+          <Col lg={4} md={6} sm={12}>
+            <ProductBox
+              title={`airpod`}
+              price={200}
+              imgSrc={`images/airpodimg.webp`}
+            />
+          </Col>
+          <Col lg={4} md={6} sm={12}>
+            <ProductBox
+              title={`airpod`}
+              price={200}
+              imgSrc={`images/airpodimg.webp`}
+            />
+          </Col>
+          <Col lg={4} md={6} sm={12}>
+            <ProductBox
+              title={`airpod`}
+              price={200}
+              imgSrc={`images/airpodimg.webp`}
+            />
+          </Col>
+          <Col lg={4} md={6} sm={12}>
+            <ProductBox
+              title={`airpod`}
+              price={200}
+              imgSrc={`images/airpodimg.webp`}
+            />
+          </Col>
+          <Col lg={4} md={6} sm={12}>
+            <ProductBox
+              title={`airpod`}
+              price={200}
+              imgSrc={`images/airpodimg.webp`}
+            />
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
+    </div>
+  );
 }
