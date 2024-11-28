b7fc096b5c0489982d61fa3a9bfb8072cfd51c44
MohamadTdn <mohamadmt1579@gmail.com>
2024-11-28 09:56:13 +0330

    start blog coding

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import NavMenu from '../../components/NavMenu/NavMenu'
+import Footer from '../../components/Footer/Footer'
 
 export default function Blog() {
   return (
-    <div>Blog</div>
+    <div className='Blog'>
+      <NavMenu/>
+      <Footer/>
+    </div>
   )
 }
