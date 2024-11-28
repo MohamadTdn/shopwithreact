492803ea628e0f05c57538d09291d487e7957920
MohamadTdn <mohamadmt1579@gmail.com>
2024-11-28 11:37:51 +0330

    finish login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import "../Register/Register.css";
+import NavMenu from "../../components/NavMenu/NavMenu";
+import Footer from "../../components/Footer/Footer";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
-    <div>
-      login
-    </div>
-  )
+    <>
+      <NavMenu />
+      <div className="Register">
+        <form action="#" id="register-form">
+          <h4 className="register-form-title">Login</h4>
+          <input
+            className="email"
+            type="email"
+            placeholder="Enter Your Email"
+          />
+          <br />
+          <input
+            className="password"
+            type="password"
+            placeholder="Enter Your Password"
+          />
+          <br />
+          <Button variant="success" type="submit">
+            Login
+          </Button>
+          <h5 className="alert-message">
+            Dont have an account ?
+            <Button variant="secondary">
+              <Link to={`/register`}>Register</Link>
+            </Button>
+          </h5>
+        </form>
+      </div>
+      <Footer />
+    </>
+  );
 }
