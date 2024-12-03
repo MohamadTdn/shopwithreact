631c755f167819ee61efc4aadbc13ae59a50779d
MohamadTdn <mohamadmt1579@gmail.com>
2024-12-03 19:33:07 +0330

    finish filter products

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,32 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProductBox from "../../components/ProductBox/ProductBox";
 import "./Products.css";
 import NavMenu from "../../components/NavMenu/NavMenu";
 import Footer from "../../components/Footer/Footer";
-import { useQuery } from "react-query";
 import api from "../../Axios/Config";
 
 export default function Products() {
-  const { data } = useQuery(["Products"], () => {
-    return api.get("/products").then((res) => {
-      return res.data;
+  const [products, setProducts] = useState([]);
+  const [orderedProducts, setOrderedProducts] = useState([]);
+  const [status, setStatus] = useState("-1");
+
+  useEffect(() => {
+    api.get("/products").then((res) => {
+      setOrderedProducts(res.data)
+      setProducts(res.data);
     });
-  });
+  }, []);
+
+  const filterProducts = (e) => {
+    setStatus(e.target.value);
+  };
+
+  useEffect(() => {
+    switch (status) {
+      case "newest": {
+        setOrderedProducts(products);
+        break;
+      }
+      case "oldest": {
+        const oldProducts = products.reverse();
+        setOrderedProducts(oldProducts);
+        break;
+      }
+      case "cheap": {
+        const cheapProducts = products.filter((product) => {
+          return product.price < 500;
+        });
+        setOrderedProducts(cheapProducts);
+        break;
+      }
+      case "expensive": {
+        const expensiveProducts = products.filter((product) => {
+          return product.price > 500;
+        });
+        setOrderedProducts(expensiveProducts);
+        break;
+      }
+      default: {
+        setOrderedProducts(products);
+
+      }
+    }
+  }, [status]);
 
   return (
     <div className="Products">
       <NavMenu></NavMenu>
-      <select className="filter-box">
+      <select onChange={filterProducts} className="filter-box">
         <option value="-1">Choose the filter</option>
-        <option value="cheap">Cheap</option>
-        <option value="expensive">Expensive</option>
-        <option value="shoes">Shoes</option>
-        <option value="gadget">Gadget</option>
+        <option value="newest">newst</option>
+        <option value="oldest">oldest</option>
+        <option value="cheap">cheap</option>
+        <option value="expensive">expensive</option>
       </select>
       <Container>
         <Row>
-          {data?.map((product) => {
+          {orderedProducts.map((product) => {
             return (
               <Col lg={4} md={6} sm={12}>
                 <ProductBox {...product} />
